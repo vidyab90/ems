cb32a1627da5d34e1d2fa4b373146ac8c7fccd88
vidyab90 <vidyab90@gmail.com>
2025-06-18 00:13:27 +0800

    Revert "my first project"
    This reverts commit a489f17cc30488e25994d52ffbfb27ed82eb48c2.

--- src/main/java/com/exampleSpringBoot/May2025/ems/repository/EmployessRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exampleSpringBoot.May2025.ems.repository;
-
-import com.exampleSpringBoot.May2025.ems.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployessRepository extends JpaRepository<Employee,Long> {
-}
